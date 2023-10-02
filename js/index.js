4390a5122d3707403e449e5e977a61e19391bcc8
greatbrain <erickfania@gmail.com>
2023-10-02 14:39:23 -0400

    Skills section redesign

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function closeWindow() {
+    var window = document.getElementById("inner-skills-window");
+    if (window){
+        window.style.display = "none";
+    }
+}
+
+function showWindow() {
+    var window = document.getElementById("inner-skills-window"); // Replace "yourDivId" with the actual ID of your div
+    if (window) {
+        window.style.display = "block";
+    }
+}
+
+function maximizeWindow() {
+    var window = document.getElementById("inner-skills-window"); // Replace "yourDivId" with the actual ID of your div
+    if (window) {
+        window.style.display = "block";
+    }
+}
